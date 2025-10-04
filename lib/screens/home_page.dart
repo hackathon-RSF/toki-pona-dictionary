bcaa6340b77b7aad1c108ca97be02563b5e85b8e
SilviaFuregato <silvia.furegato@studio.unibo.it>
2025-10-04 19:07:27 +0200

    chore: Moved homepage

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'package:toki_pona_dictionary/shared/widgets/search_bar.dart';
+import '../shared/widgets/wordsList.dart'; //
+
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0xFFC0F3FF), Color(0xFFC3FFB0)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const SizedBox(height: 100),
+              Text(
+                'Toki Pona Dictionary',
+                textAlign: TextAlign.center,
+                style: GoogleFonts.ibmPlexMono(
+                  textStyle: Theme.of(context).textTheme.displayLarge,
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 30),
+              ChangeNotifierProvider(
+                create: (_) => SearchedWord(),
+                child: const SearchBarWidget(),
+              ),
+              WordsListWidget(),
+              Text(
+                'One-day project by',
+                textAlign: TextAlign.center,
+                style: GoogleFonts.ibmPlexMono(
+                  textStyle: Theme.of(context).textTheme.displayLarge,
+                  fontSize: 16,
+                  letterSpacing: 2.0,
+                ),
+              ),
+              Text(
+                'Hackaton-RSF',
+                textAlign: TextAlign.center,
+                style: GoogleFonts.ibmPlexMono(
+                  textStyle: Theme.of(context).textTheme.displayLarge,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.0,
+                ),
+              ),
+              const SizedBox(height: 10),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
