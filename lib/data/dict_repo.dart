cbca945948287c69562d1734f21ae5ea88e993a2
Oldranda <leonardorandacio@gmail.com>
2025-10-04 20:56:45 +0200

    feat: add words to dictionary

--- lib/data/dict_repo.dart
+++ lib/data/dict_repo.dart
@@ -63,6 +63,386 @@ List<Entry> loadWords() {
         meaning: "hunting, (literally) on the hunt"
       ),
     ),
+    Entry(
+      word: "ale",
+      meanings: [
+        Meaning(
+          translations: ["all", "every", "everything", "entirety", "universe"],
+          usage: Usage.noun
+        ),
+        Meaning(
+          translations: ["one hundred"],
+          usage: Usage.number
+        ),
+      ],
+      origin: Origin(
+        word: "alle",
+        language: "Dutch",
+        meaning: "all"
+      ),
+    Entry(
+      word: "anpa",
+      meanings: [
+        Meaning(
+          translations: ["bottom", "underside", "below", "beneath", "defeated", "humble", "lowly"],
+          usage: Usage.noun
+        ),
+      ],
+      origin: Origin(
+        word: "en bas",
+        language: "Acadian French",
+        meaning: "below"
+      ),
+    ),
+    Entry(
+      word: "ante",
+      meanings: [
+        Meaning(
+          translations: ["different", "altered", "modify", "change", "other", "difference"],
+          usage: Usage.noun
+        ),
+      ],
+      origin: Origin(
+        word: "ander",
+        language: "Dutch",
+        meaning: "other, different"
+      ),
+    ),
+    Entry(
+      word: "anu",
+      meanings: [
+        Meaning(
+          translations: ["'separates multiple possibilities'", "or"],
+          usage: Usage.particle
+        ),
+      ],
+      origin: Origin(
+        word: "an",
+        language: "Georgian",
+        meaning: "or"
+      ),
+    ),
+    Entry(
+      word: "awen",
+      meanings: [
+        Meaning(
+          translations: ["stay", "remain", "wait", "pause", "protect", "keep safe", "continue"],
+          usage: Usage.verb
+        ),
+        Meaning(
+          translations: ["continue to"],
+          usage: Usage.preverb
+        ),
+      ],
+      origin: Origin(
+        word: "houden",
+        language: "Dutch",
+        meaning: "keep, care for, hold (in a particular state)"
+      ),
+    ),
+    Entry(
+      word: "e",
+      meanings: [
+        Meaning(
+          translations: ["'marks the start of a direct object'"],
+          usage: Usage.particle
+        ),
+      ],
+    ),
+    Entry(
+      word: "en",
+      meanings: [
+        Meaning(
+          translations: ["'separates multiple subjects'"],
+          usage: Usage.particle
+        ),
+      ],
+      origin: Origin(
+        word: "en",
+        language: "Dutch",
+        meaning: "and"
+      ),
+    ),
+    Entry(
+      word: "esun",
+      meanings: [
+        Meaning(
+          translations: ["trade", "barter", "exchange", "swap", "buy", "sell", "market", "shop", "fair", "bazaar", "place of business"],
+          usage: Usage.noun
+        ),
+      ],
+      origin: Origin(
+        word: "edwamu",
+        language: "Akan",
+        meaning: "at market"
+      ),
+    ),
+    Entry(
+      word: "ijo",
+      meanings: [
+        Meaning(
+          translations: ["thing", "object", "entity", "being", "matter", "phenomenon"],
+          usage: Usage.noun
+        ),
+      ],
+      origin: Origin(
+        word: "io",
+        language: "Esperanto",
+        meaning: "something"
+      ),
+    ),
+    Entry(
+      word: "ike",
+      meanings: [
+        Meaning(
+          translations: ["negative quality", "bad", "unpleasant", "harmful", "unneeded"],
+          usage: Usage.noun
+        ),
+      ],
+      origin: Origin(
+        word: "ilkeä",
+        language: "Finnish",
+        meaning: "bad, mean, wicked"
+      ),
+    ),
+    Entry(
+      word: "ilo",
+      meanings: [
+        Meaning(
+          translations: ["tool", "implement", "machine", "device"],
+          usage: Usage.noun
+        ),
+      ],
+      origin: Origin(
+        word: "ilo",
+        language: "Esperanto",
+        meaning: "tool"
+      ),
+    ),
+    Entry(
+      word: "insa",
+      meanings: [
+        Meaning(
+          translations: ["inside", "center", "between", "middle", "midpoint", "internal"],
+          usage: Usage.noun
+        ),
+      ],
+      origin: Origin(
+        word: "insait",
+        language: "Tok Pisin",
+        meaning: "inside, center, stomach"
+      ),
+    ),
+    Entry(
+      word: "jaki",
+      meanings: [
+        Meaning(
+          translations: ["disgusting", "unclean", "unsanitary", "toxic", "repulsive", "rotten"],
+          usage: Usage.noun
+        ),
+      ],
+      origin: Origin(
+        word: "yucky",
+        language: "English",
+      ),
+    ),
+    Entry(
+      word: "jan",
+      meanings: [
+        Meaning(
+          translations: ["human being", "person", "somebody"],
+          usage: Usage.noun
+        ),
+      ],
+      origin: Origin(
+        word: "jan",
+        language: "Cantonese",
+        meaning: "person",
+      ),
+    ),
+    Entry(
+      word: "jelo",
+      meanings: [
+        Meaning(
+          translations: ["yellow", "amber", "golden", "lime yellow", "yellowish orange"],
+          usage: Usage.noun
+        ),
+      ],
+      origin: Origin(
+        word: "yellow",
+        language: "English",
+      ),
+    ),
+    Entry(
+      word: "jo",
+      meanings: [
+        Meaning(
+          translations: ["hold", "carry", "possess", "contain", "own"],
+          usage: Usage.verb
+        ),
+      ],
+      origin: Origin(
+        word: "yǒu",
+        language: "Mandarin",
+        meaning: "have"
+      ),
+    ),
+    Entry(
+      word: "kala",
+      meanings: [
+        Meaning(
+          translations: ["fish", "marine animal", "sea creature", "swimming creature"],
+          usage: Usage.noun
+        ),
+      ],
+      origin: Origin(
+        word: "galama",
+        language: "Serbo-Croatian",
+        meaning: "noise (of the human voice)"
+      ),
+    ),
+    Entry(
+      word: "kama",
+      meanings: [
+        Meaning(
+          translations: ["arriving", "coming", "future", "summoned"],
+          usage: Usage.adjective
+        ),
+        Meaning(
+          translations: ["to become", "manage to", "succeed in"],
+          usage: Usage.preverb
+        ),
+      ],
+      origin: Origin(
+        word: "kamap",
+        language: "Tok Pisin",
+        meaning: "event, arrive, happen, become, bring about, summon"
+      ),
+    ),
+    Entry(
+      word: "kasi",
+      meanings: [
+        Meaning(
+          translations: ["plant", "vegetation", "herb", "leaf"],
+          usage: Usage.noun
+        ),
+      ],
+      origin: Origin(
+        word: "kasvi",
+        language: "Finnish",
+        meaning: "plant"
+      ),
+    ),
+    Entry(
+      word: "ken",
+      meanings: [
+        Meaning(
+          translations: ["to be able to", "be allowed to", "can", "may"],
+          usage: Usage.preverb
+        ),
+        Meaning(
+          translations: ["possible"],
+          usage: Usage.adjective
+        ),
+      ],
+      origin: Origin(
+        word: "ken",
+        language: "Tok Pisin",
+        meaning: "can"
+      ),
+    ),
+    Entry(
+      word: "kepeken",
+      meanings: [
+        Meaning(
+          translations: ["to use", "with", "by means of"],
+          usage: Usage.preposition
+        ),
+      ],
+      origin: Origin(
+        word: "gebruiken",
+        language: "Dutch",
+        meaning: "to use"
+      ),
+    ),
+    Entry(
+      word: "kili",
+      meanings: [
+        Meaning(
+          translations: ["fruit", "vegetable", "mushroom"],
+          usage: Usage.noun
+        ),
+      ],
+      origin: Origin(
+        word: "xili",
+        language: "Georgian",
+        meaning: "fruit"
+      ),
+    ),
+    Entry(
+      word: "kiwen",
+      meanings: [
+        Meaning(
+          translations: ["hard object", "metal", "stone", "wood"],
+          usage: Usage.noun
+        ),
+      ],
+      origin: Origin(
+        word: "kiven",
+        language: "Finnish",
+        meaning: "stone"
+      ),
+    ),
+    Entry(
+      word: "ko",
+      meanings: [
+        Meaning(
+          translations: ["semi-solid", "paste", "powder", "goo", "sand", "soil", "clay"],
+          usage: Usage.noun
+        ),
+        Meaning(
+          translations: ["squishy", "modable", "sticky"],
+          usage: Usage.adjective
+        ),
+      ],
+      origin: Origin(
+        word: "gou",
+        language: "Cantonese",
+        meaning: "cream, paste"
+      ),
+    ),
+    Entry(
+      word: "kon",
+      meanings: [
+        Meaning(
+          translations: ["air", "breath", "essence", "spirit", "hidden reality", "unseen agent"],
+          usage: Usage.noun
+        ),
+      ],
+      origin: Origin(
+        word: "kōngqì",
+        language: "Mandarin",
+        meaning: "ari, atmosphere, ambience, opinion"
+      ),
+    ),
+    Entry(
+      word: "kule",
+      meanings: [
+        Meaning(
+          translations: ["color", "pigment", "category", "genre", "flavor"],
+          usage: Usage.noun
+        ),
+        Meaning(
+          translations: ["colorful", "diverse"],
+          usage: Usage.adjective
+        ),
+      ],
+      origin: Origin(
+        word: "couleur",
+        language: "Acadian French",
+        meaning: "color"
+      ),
+    ),
   ];
 }
 
@@ -106,6 +486,4 @@ class Origin {
   String toString() => 'Origin(word: $word, language: $language, meaning: $meaning)';
 }
 
-enum Usage {
-  interject, adjective, noun, verb, particle, number, preverb
-  }
+enum Usage {interject, adjective, noun, verb, particle, number, preverb, preposition}
